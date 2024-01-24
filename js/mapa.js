603434bdd2b8e00bf460aa5240790a0f4f1b9d17
Arnold Bermell <arnoldbermell@gmail.com>
2024-01-24 13:08:38 +0100

    Api OpenStreet done

--- js/mapa.js
+++ js/mapa.js
@@ -1,29 +1,42 @@
-var map = L.map('map').setView([43.3224,-1.9850], 9);
+// Mapa
+var map = L.map('map').setView([43.1924,-2.0850], 10);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// MARCADORES
 
-// San Sebastian
-var marker = L.marker([43.3224, -1.9850]).addTo(map);
-marker.bindTooltip("<b>San Sebastian</b><br>Esta es una etiqueta pop-up.",{
-    direction : 'top',
-    offset: L.point(-15, -20)
-});
+async function crearBalizas() {
+    try {
+        const response = await fetch(`${laravelApi}/api/obtener-ubicaciones`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                'X-Requested-With': 'XMLHttpRequest'
+            },
+        });
 
-// Bilbao
-var marker = L.marker([43.2642,-2.9333]).addTo(map);
-marker.bindTooltip("<b>Bilbao</b><br>Esta es una etiqueta pop-up.",{
-    direction : 'top',
-    offset: L.point(-15, -20)
-});
+        const data = await response.json();
 
-// Eibar
-var marker = L.marker([43.1842,-2.4706]).addTo(map);
-marker.bindTooltip("<b>Bilbao</b><br>Esta es una etiqueta pop-up.",{
-    direction : 'top',
-    offset: L.point(-15, -20)
-});
+        // Por ejemplo, puedes recorrer los datos y mostrarlos en la consola
+        data.ubicaciones.forEach(async ubicacion => {
+            // Crear marcador en el mapa
+            var marker = L.marker([ubicacion.latitud, ubicacion.longitud]).addTo(map);
+            marker.bindTooltip("<b>"+ubicacion.nombre+"</b><br>",{
+                direction : 'top',
+                offset: L.point(-15, -20)
+            });
+
+            // Obtener la previsión del tiempo para la ubicación
+            const previsionResponse = await fetch(`https://api.euskalmet.euskadi.eus/weatherapi/v1/forecast?lat=${ubicacion.latitud}&lon=${ubicacion.longitud}`);
+            const previsionData = await previsionResponse.json();
+
+            // Trabajar con la previsión del tiempo (aquí solo logueo para ejemplo)
+            console.log(`Previsión para ${ubicacion.nombre}:`, previsionData);
+        });
+    } catch (error) {
+        console.error('Error al obtener datos:', error);
+    }
+}
+
 
