151d389e64fac223bf1f1717d8d5b7eff8076f77
Arnold Bermell <arnoldbermell@gmail.com>
2024-01-30 12:22:25 +0100

    Crear cards funcionando

--- js/mapa.js
+++ js/mapa.js
@@ -21,197 +21,206 @@ let vitoriaGasteiz = [];
 
 var ubicacionesGuardadas;
 
+var ubicacionesGuardadas;
+
 async function crearBalizas() {
-  try {
-      const response = await fetch(`${laravelApi}/api/obtener-ubicaciones`, {
-          method: 'GET',
-          headers: {
-              'Content-Type': 'application/json; charset=UTF-8',
-              'X-Requested-With': 'XMLHttpRequest'
-          },
-      });
+    try {
+        const response = await fetch(`${laravelApi}/api/obtener-ubicaciones`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+                'X-Requested-With': 'XMLHttpRequest'
+            },
+        });
 
-      const data = await response.json();
-      var guardados;
+        const data = await response.json();
+        var guardados;
 
-      // Eliminar eventos de clic existentes antes de agregar nuevos
-      map.off('click', 'markerClick');
+        // Eliminar eventos de clic existentes antes de agregar nuevos
+        map.off('click', 'markerClick');
 
-      // Recoger ubicaciones guardadas del Local Storage
-      guardados = localStorage.getItem('guardados');
-      ubicacionesGuardadas = guardados ? guardados.split(',') : [];
+        // Recoger ubicaciones guardadas del Local Storage
+        guardados = localStorage.getItem('guardados');
+        ubicacionesGuardadas = guardados ? guardados.split(',') : [];
 
-      data.ubicaciones.forEach(ubicacion => {
-          // Crear marcador en el mapa
-          var marker = L.marker([ubicacion.latitud, ubicacion.longitud]).addTo(map);
-          marker.bindTooltip("<b>" + ubicacion.nombre + "</b><br>", {
-              direction: 'top',
-              offset: L.point(-15, -20)
-          });
+        data.ubicaciones.forEach(ubicacion => {
+            // Crear marcador en el mapa
+            var marker = L.marker([ubicacion.latitud, ubicacion.longitud]).addTo(map);
+            marker.bindTooltip("<b>" + ubicacion.nombre + "</b><br>", {
+                direction: 'top',
+                offset: L.point(-15, -20)
+            });
 
-          // Agregar el nombre a la variable nombresUbicaciones
-          nombresUbicaciones.push(ubicacion.nombre);
+            // Agregar el nombre a la variable nombresUbicaciones
+            nombresUbicaciones.push(ubicacion.nombre);
 
-          // Verificar si la ubicación está guardada y agregar la clase
-          if (ubicacionesGuardadas.includes(ubicacion.nombre)) {
-              marker.getElement().classList.add('guardado');
-          }
+            // Verificar si la ubicación está guardada y agregar la clase
+            if (ubicacionesGuardadas.includes(ubicacion.nombre)) {
+                marker.getElement().classList.add('guardado');
+            }
 
-          // Agregar el evento de clic
-          marker.on('click', function () {
-              toggleGuardado(ubicacion.nombre, marker);
-          });
-      });
+            // Agregar el evento de clic
+            marker.on('click', function () {
+                toggleGuardado(ubicacion.nombre, marker);
+            });
+        });
 
-  } catch (error) {
-      // Manejar el error adecuadamente
-  }
+        // Generar tarjetas directamente para las ubicaciones guardadas
+        ubicacionesGuardadas.forEach(nombre => {
+            crearCard(nombre);
+        });
+
+    } catch (error) {
+        // Manejar el error adecuadamente
+    }
 }
 
 function toggleGuardado(nombre, marker) {
-  // Cambiar color
-  if (marker.getElement().classList.contains('guardado')) {
-      marker.getElement().classList.remove('guardado');
-      // Eliminar la ubicación del arreglo de guardados
-      ubicacionesGuardadas = ubicacionesGuardadas.filter(n => n !== nombre);
-  } else {
-      marker.getElement().classList.add('guardado');
-      // Agregar la ubicación al arreglo de guardados
-      ubicacionesGuardadas.push(nombre);
-  }
+    // Cambiar color
+    if (marker.getElement().classList.contains('guardado')) {
+        marker.getElement().classList.remove('guardado');
+        // Eliminar la ubicación del arreglo de guardados
+        ubicacionesGuardadas = ubicacionesGuardadas.filter(n => n !== nombre);
+    } else {
+        marker.getElement().classList.add('guardado');
+        // Agregar la ubicación al arreglo de guardados
+        ubicacionesGuardadas.push(nombre);
+    }
 
-  // Actualizar el localStorage
-  localStorage.setItem('guardados', ubicacionesGuardadas.join(','));
+    // Actualizar el localStorage
+    localStorage.setItem('guardados', ubicacionesGuardadas.join(','));
 
-  // Llamar a la función crearCard con el nombre de la ubicación
-  crearCard();
+    // Llamar a la función crearCard con el nombre de la ubicación
+    crearCard(nombre);
 }
 
-function crearCard() {
-  // Obtener el contenedor de las tarjetas
-  const ubicacionesCardsWrapper = document.querySelector('.mySwiper2 .ubicacionesCards');
+function crearCard(nombre) {
+    // Obtener el contenedor de las tarjetas
+    const ubicacionesCardsWrapper = document.querySelector('.mySwiper2 .ubicacionesCards');
 
-  // Limpiar el contenedor antes de volver a crear las tarjetas
-  ubicacionesCardsWrapper.innerHTML = '';
+    // Limpiar el contenedor antes de volver a crear las tarjetas
+    ubicacionesCardsWrapper.innerHTML = '';
 
-  // Iterar sobre las ubicaciones guardadas y crear las tarjetas
-  for (const nombre of ubicacionesGuardadas) {
-      const newSlide = document.createElement('div');
-      newSlide.className = 'swiper-slide';
-      newSlide.setAttribute('data-nombre', nombre);
+    // Iterar sobre las ubicaciones guardadas y crear las tarjetas
+    for (const nombre of ubicacionesGuardadas) {
+        const newSlide = document.createElement('div');
+        newSlide.className = 'swiper-slide';
+        newSlide.setAttribute('data-nombre', nombre);
 
-      newSlide.innerHTML = `
-      <div class="container">
-      <div class="card">
-      <div class="row">
-          <div class="col-9 left">
-              <div class="row top">
-                  <div class="col">${nombre}</div>
-                  <!-- Fecha actual -->
-                  <div class="col"><span id="fecha"></span></div>
-                  <!-- Hora actualizada cada 15 segs -->
-                  <div class="col"><span id="hora"></span></div>
-              </div>
-              <div class="row">
-                  <div class="col-7 temp">-4&deg;</div>
-                  <div class="col-5 time"><p>11:00</p><h2><b>Sábado</b></h2><p>Nublado.</p></div>
-              </div>
-              <div class="row prec ">
-                <div class="col-12"><i class="fa-solid fa-wind"></i> 6.1 mph</div>
-                <div class="col-12"><i class="fa-solid fa-droplet"></i> 90%</div>
-              </div>
-              <div class="row bottom">
-                  <div class="col"><hr></div>
-                  <div class="col border">
-                      <div class="row">Sab</div>
-                      <div class="row"><b>-4&deg;</b></div>
-                  </div>
-                  <div class="col">
-                      <div class="row">Dom</div>
-                      <div class="row"><b>-4&deg;</b></div>
-                  </div>
-                  <div class="col">
-                      <div class="row">Lun</div>
-                      <div class="row"><b>-5&deg;</b></div>
-                  </div>
-                  <div class="col">
-                      <div class="row">Mar</div>
-                      <div class="row"><b>-10&deg;</b></div>
-                  </div>
-                  <div class="col">
-                      <div class="row">Mie</div>
-                      <div class="row"><b>-4&deg;</b></div>
-                  </div>
-                  <div class="col">
-                      <div class="row">Jue</div>
-                      <div class="row"><b>-2&deg;</b></div>
-                  </div>
-                  <div class="col">
-                      <div class="row">Vie</div>
-                      <div class="row"><b>0&deg;</b></div>
-                  </div>
-                  <div class="col"><hr></div>
-              </div>
-          </div>
-          <div class="col-3 right">
-              <div class="row top" style="margin-right: 10.5em;">Gráficas</div>
+        newSlide.innerHTML = `
+        <div class="container">
+        <div class="card">
+        <div class="row">
+            <div class="col-9 left">
+                <div class="row top">
+                    <div class="col">${nombre}</div>
+                    <!-- Fecha actual -->
+                    <div class="col"><span id="fecha"></span></div>
+                    <!-- Hora actualizada cada 15 segs -->
+                    <div class="col"><span id="hora"></span></div>
+                </div>
                 <div class="row">
-
-                  <div>
-                    <div class="top text-center">
-
-                      <div class="" >
-                        <label for="desde">Desde</label>
-                        <input type="date" name="" id="desde">
-                      </div>
-
-                      
-                      <label for="hasta">Hasta</label>
-                      <input type="date" name="" id="hasta">
-                    </div>
-                    <canvas id="myChart"></canvas>
-                  </div>
-
-                  <div class="bottom da text-center">
-                    <span class="top">Dias anteriores</span>
-                      <div class="col border">
+                    <div class="col-7 temp">-4&deg;</div>
+                    <div class="col-5 time"><p>11:00</p><h2><b>Sábado</b></h2><p>Nublado.</p></div>
+                </div>
+                <div class="row prec ">
+                  <div class="col-12"><i class="fa-solid fa-wind"></i> 6.1 mph</div>
+                  <div class="col-12"><i class="fa-solid fa-droplet"></i> 90%</div>
+                </div>
+                <div class="row bottom">
+                    <div class="col"><hr></div>
+                    <div class="col border">
                         <div class="row">Sab</div>
                         <div class="row"><b>-4&deg;</b></div>
-                      </div>
-                      <div class="col border">
+                    </div>
+                    <div class="col">
                         <div class="row">Dom</div>
-                        <div class="row"><b>3&deg;</b></div>
-                      </div>
-                      <div class="col border">
+                        <div class="row"><b>-4&deg;</b></div>
+                    </div>
+                    <div class="col">
                         <div class="row">Lun</div>
-                        <div class="row"><b>-3&deg;</b></div>
-                      </div>
-                      <div class="col border">
+                        <div class="row"><b>-5&deg;</b></div>
+                    </div>
+                    <div class="col">
                         <div class="row">Mar</div>
-                        <div class="row"><b>1&deg;</b></div>
-                      </div>
-                      <div class="col border">
+                        <div class="row"><b>-10&deg;</b></div>
+                    </div>
+                    <div class="col">
                         <div class="row">Mie</div>
-                        <div class="row"><b>2&deg;</b></div>
-                      </div>
-                  </div>
-                  
-                  <script src="js/grafica.js"></script>
+                        <div class="row"><b>-4&deg;</b></div>
+                    </div>
+                    <div class="col">
+                        <div class="row">Jue</div>
+                        <div class="row"><b>-2&deg;</b></div>
+                    </div>
+                    <div class="col">
+                        <div class="row">Vie</div>
+                        <div class="row"><b>0&deg;</b></div>
+                    </div>
+                    <div class="col"><hr></div>
                 </div>
-          </div>
-      </div>
-  </div>
-      </div>
-    `;
+            </div>
+            <div class="col-3 right">
+                <div class="row top" style="margin-right: 10.5em;">Gráficas</div>
+                  <div class="row">
+  
+                    <div>
+                      <div class="top text-center">
+  
+                        <div class="" >
+                          <label for="desde">Desde</label>
+                          <input type="date" name="" id="desde">
+                        </div>
+  
+                        
+                        <label for="hasta">Hasta</label>
+                        <input type="date" name="" id="hasta">
+                      </div>
+                      <canvas id="myChart"></canvas>
+                    </div>
+  
+                    <div class="bottom da text-center">
+                      <span class="top">Dias anteriores</span>
+                        <div class="col border">
+                          <div class="row">Sab</div>
+                          <div class="row"><b>-4&deg;</b></div>
+                        </div>
+                        <div class="col border">
+                          <div class="row">Dom</div>
+                          <div class="row"><b>3&deg;</b></div>
+                        </div>
+                        <div class="col border">
+                          <div class="row">Lun</div>
+                          <div class="row"><b>-3&deg;</b></div>
+                        </div>
+                        <div class="col border">
+                          <div class="row">Mar</div>
+                          <div class="row"><b>1&deg;</b></div>
+                        </div>
+                        <div class="col border">
+                          <div class="row">Mie</div>
+                          <div class="row"><b>2&deg;</b></div>
+                        </div>
+                    </div>
+                    
+                    <script src="js/grafica.js"></script>
+                  </div>
+            </div>
+        </div>
+    </div>
+        </div>
+      `;
 
-      ubicacionesCardsWrapper.appendChild(newSlide);
-  }
+        ubicacionesCardsWrapper.appendChild(newSlide);
+    }
 
-  // Actualizar el swiper
-  swiper2.update();
+    // Actualizar el swiper
+    swiper2.update();
 }
 
+// Llamar a crearBalizas al cargar la página o en el momento adecuado
 crearBalizas();
+
 
 
 
